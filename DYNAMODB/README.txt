590c33f369823783bb88584e1d516ae967218a04
Aravindan Palani <aravindan.palani@vm1-hub.msf.ad>
2022-05-20 17:19:42 +0530

    DynamoDB_Opertaions

---
+++ DYNAMODB/README.txt
@@ -0,0 +1,5 @@
+This Example is to how to connect AWS DynamoDB
+1.Create table
+2.Insert table
+3.Delete table
+4.Update table
--- DYNAMODB/dynamodb_operations.go
+++ DYNAMODB/dynamodb_operations.go
@@ -1,1 +1,184 @@
+package main
 
+import (
+	"fmt"
+	"log"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+type Item struct {
+	Username string
+	Id int
+	Address string
+}
+
+func main() {
+	fmt.Printf("Enter a Option : ")
+	var input int
+	fmt.Scanln(&input)
+	switch input {
+	case 1:
+		createtable()
+	case 2:
+		inserttable()
+	case 3:
+		deletedata()
+	case 4:
+		updatetable()
+	}
+}
+
+func createtable() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := dynamodb.New(sess)
+
+	tableName := "Test_DB"
+
+	input := &dynamodb.CreateTableInput{
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("Username"),
+				AttributeType: aws.String("S"),
+			},
+			{
+				AttributeName: aws.String("Id"),
+				AttributeType: aws.String("N"),
+			},
+		},
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("Username"),
+				KeyType:       aws.String("HASH"),
+			},
+			{
+				AttributeName: aws.String("Id"),
+				KeyType:       aws.String("RANGE"),
+			},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(2),
+			WriteCapacityUnits: aws.Int64(2),
+		},
+		TableName: aws.String(tableName),
+	}
+
+	_, err := svc.CreateTable(input)
+	if err != nil {
+		log.Fatalf("Error Creating Table: %s", err)
+	}
+
+	fmt.Println("Created the table", tableName)
+
+}
+
+func inserttable() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := dynamodb.New(sess)
+
+	item := Item{
+		Username : "Jhon",
+		Id : 1,
+		Address : "Chennai",
+	}
+
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		log.Fatalf("Error marshalling new item: %s", err)
+	}
+
+	tableName := "Test_DB"
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = svc.PutItem(input)
+	if err != nil {
+		log.Fatalf("Error calling PutItem: %s", err)
+	}
+
+	fmt.Println("Successfully added ")
+
+}
+
+func deletedata() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := dynamodb.New(sess)
+
+	tableName := "Test_DB"
+	name := "Jhon"
+	id := "1"
+
+	input := &dynamodb.DeleteItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"Id": {
+				N: aws.String(id),
+			},
+			"Username": {
+				S: aws.String(name),
+			},
+		},
+		TableName: aws.String(tableName),
+	}
+
+	_, err := svc.DeleteItem(input)
+	if err != nil {
+		log.Fatalf("Got error calling DeleteItem: %s", err)
+	}
+
+
+	fmt.Println("Deleted '" + name + "from table " + tableName)
+
+}
+
+func updatetable() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := dynamodb.New(sess)
+
+	tableName := "Test_DB"
+	name := "Jhon"
+	id := "1"
+	address := "Bangalore"
+
+	input := &dynamodb.UpdateItemInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":Address": {
+				S: aws.String(address),
+			},
+		},
+		TableName: aws.String(tableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"Id": {
+				N: aws.String(id),
+			},
+			"Username": {
+				S: aws.String(name),
+			},
+		},
+		ReturnValues:     aws.String("UPDATED_NEW"),
+		UpdateExpression: aws.String("set Address = :Address"),
+	}
+
+	_, err := svc.UpdateItem(input)
+	if err != nil {
+		log.Fatalf("Got error calling UpdateItem: %s", err)
+	}
+
+	fmt.Println("Successfully updated ")
+}
