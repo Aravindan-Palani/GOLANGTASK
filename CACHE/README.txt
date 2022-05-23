fdd3caaa8dd7f601b0d39266ca71fc8989e7fb84
Aravindan Palani <aravindan.palani@vm1-hub.msf.ad>
2022-05-23 09:09:32 +0530

    cache

---
+++ CACHE/README.txt
@@ -0,0 +1,1 @@
+This program is how to cache the data using cache package.
--- CACHE/cache.go
+++ CACHE/cache.go
@@ -1,1 +1,37 @@
+package main
 
+import(
+        "fmt"
+        "github.com/patrickmn/go-cache"
+        "time"
+)
+
+type Example struct {
+        Name string
+        Age int
+}
+
+func main() {
+        newExample := &Example {
+                Name: "jhon",
+                Age: 12,
+        }
+        c := cache.New(5*time.Minute, 10*time.Minute)
+
+        foo, found := c.Get("foo")
+        if found {
+                a := foo.([]Example)
+                a = append(a, *newExample)
+                c.Set("foo", a, cache.NoExpiration)
+                fmt.Println("Inside If")
+        } else {
+                c.Set("foo", newExample, cache.NoExpiration)
+                fmt.Println("Inside else")
+
+        }
+
+        foo1, found := c.Get("foo")
+        if found {
+                fmt.Println(foo1)
+        }
+}
